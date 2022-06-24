90d302cc4d4e0a966a9befb840e6b318d43ffb66
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-25 02:53:54 +0700

    service

--- src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
+++ src/main/java/com/example/springsecurityjwt/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.springsecurityjwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User ,Integer> {
+    User findByUsername(String username);
 }
