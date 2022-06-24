1b7acdba77ebb374b05c999dc97f27e97addc09d
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-25 00:55:11 +0700

    create

---
+++ src/main/java/com/example/springsecurityjwt/SecurityjwyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityjwyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityjwyApplication.class, args);
+	}
+
+}
